7f0e9f4fd6e82581ed5bae0382ef752590cb211b
maximeraoust <maximeraoust@b02a4dee-eab9-7e40-38f2-99572a7f6a5c>
2011-06-10 11:32:49 +0000

    Updated examples:  * player.bat now prompts to enter the API credentials  * added maniahome.bat to test the posting of notifications on ManiaHome!

--- examples/player.bat
+++ examples/player.bat
@@ -1,21 +1,19 @@
 @ECHO OFF
-ECHO ***********************************************************************
-ECHO *                                                                     *
-ECHO *         ManiaLib - Lightweight PHP framework for Manialinks         *
-ECHO *                                                                     *
-ECHO *                              ---------                              *
-ECHO *                                                                     *
-ECHO *         This source file is subject to the LGPL License 3           *
-ECHO *         It is available through the world-wide-web                  *
-ECHO *         at this URL:  http://www.gnu.org/licenses/lgpl.html         *
-ECHO *                                                                     *
-ECHO *         Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)        *
-ECHO *                                                                     *
-ECHO *         Usage:                                                      *
-ECHO *         Rename the file to "script_name.bat" and run it             *
-ECHO *         It will try to execute "php script_name.php"                *
-ECHO *                                                                     *
-ECHO ***********************************************************************
+
+REM ****************************************************************************
+REM *
+REM * ManiaLib - Lightweight PHP framework for Manialinks
+REM *
+REM * This source file is subject to the LGPL License 3. It is available through 
+REM * the world-wide-web at this URL:  http://www.gnu.org/licenses/lgpl.html
+REM *
+REM * Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
+REM *
+REM * Usage:
+REM * Rename the file to "script_name.bat" and run it.
+REM * It will try to execute "php.exe script_name.php".
+REM *
+REM ****************************************************************************
 
 SET filename=%0
 FOR %%F IN (%filename%) DO SET dirname=%%~dpF
